9a30e3e550de6aa852bcb2baa3c94a7b5b676cb5
Sebastian Fey <info@sebastianfey.de>
2024-02-21 20:00:00 +0100

    feat: Use new classes in Recipe
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

--- src/Entities/SchemaOrg/Recipe.php
+++ src/Entities/SchemaOrg/Recipe.php
@@ -2,104 +2,141 @@
 
 namespace RecipeImportPipeline\Entities\SchemaOrg;
 
-use RecipeImportPipeline\Exceptions\JsonMappingException;
-use RecipeImportPipeline\Utilities\JsonMapper;
-use RecipeImportPipeline\Utilities\TypeUtilities;
+use RecipeImportPipeline\Entities\SchemaOrg\Utility\Ingredients;
+use RecipeImportPipeline\Entities\SchemaOrg\Utility\Instructions;
+use RecipeImportPipeline\Entities\SchemaOrg\Utility\Keywords;
+use RecipeImportPipeline\Entities\SchemaOrg\Utility\PlainText;
+use RecipeImportPipeline\Entities\SchemaOrg\Utility\Supplies;
 
 /**
  * A recipe object.
  */
-class Recipe extends BaseSchemaOrgEntity
-{
-    /**
-     * @inheritDoc
-     */
-    protected static array $RequiredProperties = ['name'];
+class Recipe {
+    /** @var PlainText|null $identifier The identifier of the recipe. */
+    private ?PlainText $identifier;
+
+    /** @var PlainText|null $name The name of the recipe. */
+    private ?PlainText $name;
+
+    /** @var PlainText|null $dateCreated The creation date of the recipe. */
+    private ?PlainText $dateCreated;
+
+    /** @var Keywords|null $keywords The keywords associated with the recipe. */
+    private ?Keywords $keywords;
+
+    /** @var PlainText|null $totalTime The total time required for the recipe. */
+    private ?PlainText $totalTime;
+
+    /** @var Ingredients|null $recipeIngredient The ingredients needed for the recipe. */
+    private ?Ingredients $recipeIngredient;
+
+    /** @var Instructions|null $recipeInstructions The instructions for preparing the recipe. */
+    private ?Instructions $recipeInstructions;
+
+    /** @var Supplies|null $supply The supplies required for the recipe. */
+    private ?Supplies $supply;
 
     /**
-     * @var string Unique identifier of the recipe.
+     * Constructor for the Recipe class.
+     *
+     * @param PlainText|null $identifier The identifier of the recipe.
+     * @param PlainText|null $name The name of the recipe.
+     * @param PlainText|null $dateCreated The creation date of the recipe.
+     * @param Keywords|null $keywords The keywords associated with the recipe.
+     * @param PlainText|null $totalTime The total time required for the recipe.
+     * @param Ingredients|null $recipeIngredient The ingredients needed for the recipe.
+     * @param Instructions|null $recipeInstructions The instructions for preparing the recipe.
+     * @param Supplies|null $supply The supplies required for the recipe.
      */
-    private string $Id;
-
-    /**
-     * @var string Name of the recipe.
-     */
-    public string $Name;
-
-    /**
-     * @var array<HowToSupply> List of supplies for the recipe.
-     */
-    public array $Supply;
-
-    /**
-     * @var array<HowToSection|HowToStep> List of directions for the recipe.
-     */
-    public array $RecipeInstructions;
-
-    /**
-     * Recipe constructor.
-     * @param string $Id
-     * @param string $Name
-     * @param array $Supply
-     * @param array $RecipeInstructions
-     */
-    public function __construct(string $Id, string $Name, array $Supply, array $RecipeInstructions)
-    {
-        $this->Id = $Id;
-        $this->Name = $Name;
-        $this->Supply = $Supply;
-        $this->RecipeInstructions = $RecipeInstructions;
+    public function __construct(
+        ?PlainText $identifier,
+        ?PlainText $name,
+        ?PlainText $dateCreated,
+        ?Keywords $keywords,
+        ?PlainText $totalTime,
+        ?Ingredients $recipeIngredient,
+        ?Instructions $recipeInstructions,
+        ?Supplies $supply,
+    ) {
+        $this->identifier = $identifier;
+        $this->name = $name;
+        $this->dateCreated = $dateCreated;
+        $this->keywords = $keywords;
+        $this->totalTime = $totalTime;
+        $this->recipeIngredient = $recipeIngredient;
+        $this->recipeInstructions = $recipeInstructions;
+        $this->supply = $supply;
     }
 
     /**
-     * @return string
+     * Get the identifier of the recipe.
+     *
+     * @return PlainText|null The identifier of the recipe.
      */
-    public function getId(): string
-    {
-        return $this->Id;
+    public function getIdentifier(): ?PlainText {
+        return $this->identifier;
     }
 
     /**
-     * @inheritDoc
+     * Get the name of the recipe.
+     *
+     * @return PlainText|null The name of the recipe.
      */
-    public static function fromJson(array $json): ?BaseSchemaOrgEntity
-    {
-        // Validate required fields
-        if (!Recipe::checkRequiredProperties($json)) {
-            return null;
-        }
+    public function getName(): ?PlainText {
+        return $this->name;
+    }
 
-        try {
-            // Extract Id and Name
-            $id = JsonMapper::ExtractString($json['identifier']);
-            $name = JsonMapper::ExtractString($json['name']);
+    /**
+     * Get the creation date of the recipe.
+     *
+     * @return PlainText|null The creation date of the recipe.
+     */
+    public function getDateCreated(): ?PlainText {
+        return $this->dateCreated;
+    }
 
-            // Extract supplies
-            $supply = [];
-            if (isset($json['recipeIngredient'])) {
-                $supply = JsonMapper::ExtractStringArray($json['recipeIngredient']);
-            }
+    /**
+     * Get the keywords associated with the recipe.
+     *
+     * @return Keywords|null The keywords associated with the recipe.
+     */
+    public function getKeywords(): ?Keywords {
+        return $this->keywords;
+    }
 
-            // Extract recipe instructions
-            $recipeInstructions = [];
-            if (isset($json['recipeInstructions'])) {
-                $instructions = TypeUtilities::as_cleaned_array($json['recipeInstructions']);
-                foreach ($instructions as $instruction) {
-                    if (JsonMapper::isSchemaObject($instruction, 'HowToSection', false)) {
-                        $recipeInstructions[] = HowToSection::fromJson($instruction);
-                    } elseif (JsonMapper::isSchemaObject($instruction, 'HowToStep', false)) {
-                        $recipeInstructions[] = HowToStep::fromJson($instruction);
-                    } elseif (JsonMapper::isSchemaObject($instruction, 'HowToDirection', false)) {
-                        $recipeInstructions[] = HowToDirection::fromJson($instruction);
-                    } else {
-                        $recipeInstructions[] = $instruction;
-                    }
-                }
-            }
+    /**
+     * Get the total time required for the recipe.
+     *
+     * @return PlainText|null The total time required for the recipe.
+     */
+    public function getTotalTime(): ?PlainText {
+        return $this->totalTime;
+    }
 
-            return new Recipe($id, $name, $supply, $recipeInstructions);
-        } catch (JsonMappingException) {
-            return null;
-        }
+    /**
+     * Get the ingredients needed for the recipe.
+     *
+     * @return Ingredients|null The ingredients needed for the recipe.
+     */
+    public function getRecipeIngredient(): ?Ingredients {
+        return $this->recipeIngredient;
+    }
+
+    /**
+     * Get the instructions for preparing the recipe.
+     *
+     * @return Instructions|null The instructions for preparing the recipe.
+     */
+    public function getRecipeInstructions(): ?Instructions {
+        return $this->recipeInstructions;
+    }
+
+    /**
+     * Get the supplies required for the recipe.
+     *
+     * @return Supplies|null The supplies required for the recipe.
+     */
+    public function getSupply(): ?Supplies {
+        return $this->supply;
     }
 }
